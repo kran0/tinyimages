85dfb06a721d8155a770951e3a5e49a97e15bcc1
Pavel Shumskii <shumskii@gmail.com>
2022-05-14 17:22:23 +0300

    fix huge bug

--- apkextractor.sh
+++ apkextractor.sh
@@ -6,38 +6,37 @@
 # USAGE: $0 PACKAGE [PACKAGE] ... [PACKAGE]
 #
 
-[ -z "$1" ]      && exit 0            #We need packages names in $@
-[ -z "$TARGET" ] && TARGET="/target"
-mkdir -p "$TARGET"
+[ -z "${1}" ]      && exit 0            #We need packages names in ${@}
+[ -z "${TARGET}" ] && TARGET='/target'
+mkdir -p "${TARGET}"
 
+function getDependsOn {                 #Find full number of depencies
 
-function getDependsOn {               # find full number of depencies
+ [ -z "${1}" ] && exit 0
 
- [ -z "$1" ] && exit 0
-
- if [ -z "$SEENFILE" ]
+ if [ -z "${SEENFILE}" ]
  then
-  SEENFILE=$(mktemp)
-  printf '%s\n' "$@" > $SEENFILE
+  SEENFILE="$(mktemp)"
+  printf '%s\n' ${@} > "${SEENFILE}"
+  trap 'rm "${SEENFILE}"' EXIT
  fi
 
- DEPENDSON=$(apk info -R "$@"\
-             | sed -e '/ depends on:$/d'\
-             | grep -Fxvf "$SEENFILE"\
-             | tee -a "$SEENFILE")
+ DEPENDSON=$(apk info -R ${@}\
+             | sed -e '/ depends on:$/d;/^[[:space:]]*$/d'\
+             | grep -Fxvf "${SEENFILE}"\
+             | tee -a "${SEENFILE}")
 
- printf '%s\n' "$@" "$DEPENDSON" $(getDependsOn "$DEPENDSON")
- [ -f "$SEENFILE" ] && rm "$SEENFILE"
+ printf '%s\n' ${@} ${DEPENDSON} $(getDependsOn ${DEPENDSON})
 }
 
 function getContains {    # find unique file names containing in given packages
  (
 
- [ -z "$ADDFILES" ]\
-  || printf '%s\n' $ADDFILES
+ [ -z "${ADDFILES}" ]\
+  || printf '%s\n' ${ADDFILES}
 
- [ -z "$1" ]\
-  || apk info -L "$@" | sed -e '/ contains:$/d'
+ [ -z "${1}" ]\
+  || apk info -L ${@} | sed -e '/ contains:$/d'
 
  )\
   | sed -e '/^[[:space:]]*$/d'\
@@ -47,6 +46,10 @@ function getContains {    # find unique file names containing in given packages
 
 # MAIN call
 
-getContains $(getDependsOn "$@" | sort -u)\
+apk add --quiet --update --virtual .apkextractor ${@}
+
+getContains $(getDependsOn ${@} | sort -u)\
  | tar vc -C / -T -\
- | tar x -C "$TARGET"
+ | tar x -C "${TARGET}"
+
+apk del .apkextractor
